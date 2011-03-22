311eb0b601228a9ceb4fe8bed536829403d9042e
clime7@gmail.com <clime7@gmail.com@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-03-22 22:53:33 +0000

    AI baby import

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "net.h"
+
+using namespace std;
+
+/*
+ * suggested parameters:
+ * 	-net <file path> - load a network from the file; if omitted, a new network is created
+ * 	-train <file path> - train a network on cases in the file and print the resulting network to stdout
+ * 	-run <file path> - run a network on cases in the file; print the results to stdout
+ */
+int main(int argc, char **argv)
+{
+	return 0;
+}
---
+++ neuron.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <utility>
+
+using namespace std; 
+
+class Neuron {
+	private:
+		struct link {
+			double weight;
+			Neuron& parent;
+		};
+	
+		vector<link> parents;
+		double input; //relevant if this neuron is an input neuron
+		double (*act_func)(double); //pointer to an activation function
+	public:
+		//TODO: constructor: Neuron(...);
+		void set_input(double val);
+		void set_act_func(double (*act_func)(double));
+		double output(); //depends on outputs from its parents, weights, and an activation function
+};
