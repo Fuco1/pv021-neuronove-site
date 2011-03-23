a9d9adc9744eac963076c287acf4bb9bf960b7be
172454@gmail.com <172454@gmail.com@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-03-23 09:19:25 +0000

    Neuron::get_output. Image and Net changed to class templates (template parameter being the voxel data type).

--- trunk/image.h
+++ trunk/image.h
@@ -4,13 +4,13 @@
 #include <iostream>
 #include <vector>
 
-class Image {
+template <class VoxelType> class Image {
 	private:
 		int width;
 		int height;
 		int depth;
 
-		std::vector<int> data;
+		std::vector<VoxelType> data;
 	public:
 		int getWidth(void) {
 			return width;
@@ -22,7 +22,7 @@ class Image {
 			return depth;
 		};
 
-		int getVoxel(int x, int y, int z) {
+		VoxelType getVoxel(int x, int y, int z) {
 			return data[x + getWidth() * y + getWidth() * getHeight() * z];
 		}
 
