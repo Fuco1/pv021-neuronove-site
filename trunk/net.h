b099d5e59afec58a0827e1bc2fdabbea4474aa82
172454@gmail.com <172454@gmail.com@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-03-23 09:38:19 +0000

    Net and Image save/load interface.

--- trunk/net.h
+++ trunk/net.h
@@ -10,12 +10,15 @@ using namespace std;
 
 template <class VoxelType> class Net {
 	private:
-		vector<Neuron> neurons;
-		Neuron& head;
+		vector<Neuron> neurons; // All neurons of this net.
+		Neuron *head; //Neuron &head; -- Opravdu se tu mela pouzit reference misto ukazatele? Mne to prijde jako chyba, proto jsem to zakomentoval. Karel
 	public:
 		//TODO: constructor: Net(...);
 		bool run(const Image<VoxelType> &img);
 		void train(const Image<VoxelType> &img, bool exp_val);
+
+		void saveToFile(const std::string &fileName); // \todo Save the net to a file.
+		void loadFromFile(const std::string &fileName);// \todo Load the net from a file.
 };
 
 #endif // NET_H_INCLUDED
