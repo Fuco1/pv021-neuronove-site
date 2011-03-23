66c87721a9894c8ef0eceac2d266001b707b8bf5
clime7 <clime7@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-03-23 13:18:07 +0000

    cosmetic changes, bool replaced for double in Neuron and Net

---
+++ net.cpp
@@ -0,0 +1,17 @@
+#include "net.h"
+
+double Neuron::run(const Image<VoxelType> &img) {
+    //TODO
+}
+		
+void Neuron::train(const Image<VoxelType> &img, double exp_val) {
+    //TODO
+}
+
+void Neuron::saveToFile(const std::string &fileName) {
+    //TODO
+} 
+
+void Neuron::loadFromFile(const std::string &fileName) {
+    //TODO
+}
--- neuron.cpp
+++ neuron.cpp
@@ -1,1 +1,30 @@
+#include "neuron.h"
 
+double Neuron::get_output() {
+    // If this neuron has no parent...
+    if (parents.size() == 0) {
+        // ...just return the value (parent-less neurons work as input).
+        return input;
+    // Otherwise...
+    } else {
+        // ...calculate the weighted sum of its inputs...
+        double weightedSum = 0;
+        for (vector<link>::iterator i = parents.begin(); i != parents.end(); ++i) {
+            if (i->neuron == NULL) {
+                weightedSum += i->weight; // Return weight_k * 1 if the link is NULL; this will be the case with the 0-th link, which will be representing the neuron's bias (x_0 = 1).
+            } else {
+                weightedSum += i->weight * i->neuron->get_output(); // Return weight_k * x_k
+            }
+        }
+        // ...and apply the activation function.
+        return act_func(weightedSum);
+    }
+}
+
+void Neuron::set_input(double input) {
+    this->input = val;
+}
+
+void Neuron::set_act_func(double (*act_func)(double)) {
+    this->act_func = act_func;
+}
