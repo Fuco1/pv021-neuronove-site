57610ebf1c68617852a0ef1a0e27e48c3c8af642
clime7 <clime7@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-04-07 21:50:44 +0000

    init func added to hold shared code in constructors and loadFromFile method

--- net.h
+++ net.h
@@ -16,8 +16,8 @@ class Net {
 	public:
 		std::vector<Layer> layers; // All neurons of this net.
 	public:
-		// \todo Constructor
-		Net(const std::vector<size_t> &neuronCounts) {
+		// shared code for constructors
+		void init(const std::vector<size_t> &neuronCounts) {
 			Layer currentLayer;
 			for (size_t i = 0; i < neuronCounts.size(); ++i) {
 				currentLayer.clear();
@@ -27,9 +27,14 @@ class Net {
 				}
 				layers.push_back(currentLayer);
 			}
-		};
-
-
+		}
+		
+		// nonparametric constructor
+		Net() {}	
+		
+		Net(const std::vector<size_t> &neuronCounts) {
+			init(neuronCounts);
+		}
 
 		double run(const Image<double> &image) {
 			if (image.getSize() != layers[0].size()) {
@@ -38,7 +43,7 @@ class Net {
 			}
 
 			// Initialize all potentials (either to the neuron's bias, or -- in the case of input neurons -- to the corresponding input value).
-      for (size_t layerIndex = 0; layerIndex < layers.size(); ++layerIndex) {
+			for (size_t layerIndex = 0; layerIndex < layers.size(); ++layerIndex) {
 				for (size_t neuronIndex = 0; neuronIndex < layers[layerIndex].size(); ++neuronIndex) {
 					if (layerIndex == 0) {
 						layers[layerIndex][neuronIndex].setPotential(image.getVoxel(neuronIndex));
@@ -46,32 +51,26 @@ class Net {
 						layers[layerIndex][neuronIndex].setPotential(layers[layerIndex][neuronIndex].getInputWeight(0));
 					}
 				}
-      }
+			}
 
-      // Propagate the values from layer 0 upwards.
-      for (size_t layerIndex = 0; layerIndex < layers.size() - 1; ++layerIndex) {
+			// Propagate the values from layer 0 upwards.
+			for (size_t layerIndex = 0; layerIndex < layers.size() - 1; ++layerIndex) {
 				for (size_t neuronIndex = 0; neuronIndex < layers[layerIndex].size(); ++neuronIndex) {
 					layers[layerIndex][neuronIndex].propagateValueToUpperLayer();
 				}
-      }
+			}
 
-      // All potentials have been set; return the value of the output neuron.
-      double returnValue = layers[layers.size() - 1][0].getValue();
-      std::cout << "Run finished with return value = " << returnValue << std::endl;
-      return returnValue;
-		};
-
+			// All potentials have been set; return the value of the output neuron.
+			double returnValue = layers[layers.size() - 1][0].getValue();
+			std::cout << "Run finished with return value = " << returnValue << std::endl;
+			return returnValue;
+		}
 
 		void train(const Image<double> &img, double expectedValue); // will call run() as one (probably the first) step
 
-		// \todo Save the net to a file.
-		void saveToFile(const std::string &fileName) {
-		};
+		void saveToFile(const char* fileName);
 
-		// \todo Load the net from a file.
-		void loadFromFile(const std::string &fileName) {
-		};
-
+		bool loadFromFile(const char* fileName);
 };
 
 
