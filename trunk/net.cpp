d955bab192548f035f6db9b5d1a437f53547ad96
clime7 <clime7@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-04-07 21:51:38 +0000

    saveToFile/loadFromFile implemented

--- trunk/net.cpp
+++ trunk/net.cpp
@@ -1,9 +1,69 @@
 #include "cmath"
 #include "net.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 double actFuncTanh(double potential) {
 	const double a = 1.7159;
 	const double b = 2.0/3.0;
 
 	return a * tanh(b * potential);
-};
+}
+
+/* 
+ * Save the net to a file. 
+ * Params: 
+ * 		filename - name of the file to load a network from
+ */
+void Net::saveToFile(const char* fileName) {
+	ofstream file(fileName);
+	file << this->layers.size() << endl;
+	for (size_t x = 0; x < this->layers.size(); ++x) {
+		file << this->layers[x].size() << endl;
+	}
+	for (size_t x = 0; x < this->layers.size(); ++x) {
+		for (size_t y = 0; y < this->layers[x].size(); ++y) {
+			Neuron &neuron = this->layers[x][y];
+			for (size_t z = 0; z < neuron.getInputWeightsSize(); ++z) {
+				file << neuron.getInputWeight(z) << " ";
+			}
+			file << endl;
+		}
+		file << "-----" << endl;
+	}
+}
+
+/* 
+ * Load the net from a file. Lacks checks for the file format.
+ * Params: 
+ * 		filename - name of the file to load a network from
+ * Returns: false if the file does not exist
+ */
+bool Net::loadFromFile(const char* fileName) {
+	ifstream file(fileName);
+	if (file.fail()) return false;
+	size_t ilayers;
+	file >> ilayers;
+	vector<size_t> neuronCounts;
+	for (size_t x = 0; x < ilayers; ++x) {
+		size_t ineurons;
+		file >> ineurons;
+		neuronCounts.push_back(ineurons);
+	}
+	this->init(neuronCounts);
+	for (size_t x = 0; x < this->layers.size(); ++x) {
+		for (size_t y = 0; y < this->layers[x].size(); ++y) {
+			Neuron &neuron = this->layers[x][y];
+			for (size_t z = 0; z < neuron.getInputWeightsSize(); ++z) {
+				double weight;
+				file >> weight;
+				neuron.setInputWeight(z, weight);
+			}
+		}
+		string delimiter;
+		file >> delimiter;	
+	}
+	return true;
+}
