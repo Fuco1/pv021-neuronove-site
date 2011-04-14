f87f0944d995cd571a708eecea97a0e62a625a7c
clime7 <clime7@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-04-14 22:53:50 +0000

    parsing cmd line sketch

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,42 +1,150 @@
 #include <iostream>
+#include <getopt.h>
+#include <sstream>
+#include <string>
 #include "net.h"
 #include "neuron.h"
 #include "image.h"
 
 using namespace std;
 
-/*
- * suggested parameters:
- *  -createnet <x> <y> <z> - create a net with the given parameters
- * 	-loadnet <file path> - load a network from the file
- * 	-savenet <file path> - save the network to the file
- * 	-train <directory with positives> <directory with negatives> - train a network on cases in the directories
- * 	-run <directory with real images> - run an already trained network on cases in the directory; print the results to stdout
-
- example:
- a.exe -createnet 10 10 20 -savenet network.dat                                          // vytvori novou netrenovanou sit pro 10x10x20 voxelu
- a.exe -loadnet network.dat -savenet network.dat -train ./positives ./negatives          // nacte sit, vytrenuje a ulozi pod stejnym nazvem
- a.exe -loadnet network.dat -run ./realdata                                              // nacte sit, provede vypocty nad realnymi daty
- */
+// singleton struct to save values given by user
+struct UserOptions {
+	vector<LayerSpec> netSpec;
+	char *loadPath;
+	char *savePath;
+	char *trainPath;
+	char *runPath;
+	double rate;
+	unsigned int iters;
+	// default values
+	UserOptions() {
+		loadPath = NULL;
+		savePath = NULL;
+		trainPath = NULL;
+		runPath = NULL;
+		rate = 0.05;
+		iters = 1;
+	}
+} userOptions;
 
 // Returns the vector of all filenames in the given directory
 std::vector<std::string> getFileNames(const std::string &directoryName) {
 	std::vector<std::string> fileNames;
 	#ifdef WIN_32
-  // \todo windows
+	// \todo windows
 	#else
-  // \todo unix
+	// \todo unix
 	#endif
 	return fileNames;
 }
 
+void printHelp() {
+	cerr << "Program options:\n\
+	-c, --createnet <x0>:<func0>,<x1>:<func1>,<x2>:<func2>,...\n\
+		create a network with <xi> neurons in i-th layer such that\n\
+		i-th layer uses <funci>, which is name of an activation function\n\
+		identity funcion (id) should be used for 0-th layer\n\
+	-l, --loadnet <file path>\n\
+		load a network from the file\n\
+	-s, --savenet <file path>\n\
+		save the network to the file\n\
+	-t, --train <file path>\n\
+		train a network on train cases in the file/directory\n\
+	-i, --iters <x>\n\
+		number of training iterations over the whole data set; default is 1\n\
+	-x, --rate <x>\n\
+		how learning rate to be used for training; default is 0.05\n\
+	-r, --run <file path>\n\
+		run an already trained network on real cases in the file/directory\n\
+	-h, --help\n\
+		print this help\n";
+}
+
+/*
+EXAMPLES:
+	 a.exe --createnet 2000:id,20:tanh,1:unitstep --savenet network.dat
+	 	Vytvori novou netrenovanou dvou-vrstvou sit s
+		2000 vstupnimi, 20 skrytymi, a jednim vystupnim neuronem.
+	 	Vstupni vrstva ma pridelenu identickou aktivacni funkci, 
+		skryta ma tanh, a vystupni vrstve nalezi funkce unitstep.
+		Sit pote ulozi do souboru network.dat
+	 a.exe -c 2000:id,20:tanh,1:unitstep -s network.dat
+	 	stejne jako predchozi priklad s kratkymi verzemi parametru
+	 a.exe --loadnet network.dat --savenet network.dat --train ./traindata -i 2 -x 0.1
+	 	Nacte sit, vytrenuje a ulozi pod stejnym nazvem. Trenovaci
+		data se projdou 2x s rychlosti uceni 0,1.
+	 a.exe --loadnet network.dat --run ./realdata
+	 	nacte sit, provede vypocty nad realnymi daty
+*/
+
 int main(int argc, char **argv) {
+	// parse command line options
+	int c;
+	while (true) {
+		static struct option long_options[] =
+		{
+			{"createnet", required_argument, 0, 'c'},
+			{"loadnet",   required_argument, 0, 'l'},
+			{"savenet",   required_argument, 0, 's'},
+			{"train",     required_argument, 0, 't'},
+			{"iters",     required_argument, 0, 'i'},
+			{"rate",      required_argument, 0, 'x'},
+			{"run",       required_argument, 0, 'r'},
+			{0, 0, 0, 0}
+		};
+		
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+
+		c = getopt_long (argc, argv, "c:l:s:t:i:x:r:",
+						long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 'c':
+				stringstream ssline(optarg);
+				string item, first, second;
+				while (getline(ssline, item, ',')) {
+					if (item.size() == 0) continue;
+					stringstream  ssitem(item);
+					getline(ssitem, first, ':');
+					getline(ssitem, second, ':');
+					//TODO
+				}
+				break;
+			case 'l':
+				userOptions.loadPath = optarg;
+				break;
+			case 's':
+				userOptions.savePath = optarg;
+				break;
+			case 't':
+				userOptions.trainPath = optarg;
+				break;
+			case 'i':
+				userOptions.iters = atoi(optarg);
+				break;
+			case 'x':
+				userOptions.rate = atof(optarg);
+				break;
+			case 'r':
+				userOptions.runPath = optarg;
+				break;
+			default:
+				printHelp();
+				exit(1);
+		}
+	}
 	Image<double> image("image1.dat");
 
 	/*
 	Net net;
 
-	if (!net.loadFromFile("./test.net")) {
+	if (!net.loadFromFile("./network.dat")) {
 		cerr << "File does not exist. Bye bye." << endl;
 		exit(1);
 	}
@@ -99,7 +207,7 @@ int main(int argc, char **argv) {
 		std::cout << "net output is " << net.run(input) << std::endl;
 	} while (inputValue != 0);
 
-	net.saveToFile("./test.net");
+	net.saveToFile("./network.dat");
 
 	return 0;
 }
