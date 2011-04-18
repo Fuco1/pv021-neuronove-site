fd28aa023c9ec4ef720413a4ecc3d6276e2f3dc3
172454@gmail.com <172454@gmail.com@a70358c5-60c5-5d4a-5aef-88f6f0ca0020>
2011-04-18 13:19:20 +0000

    Image loading implemented.
    DataItem class added as a superclass of Image.

--- image.h
+++ image.h
@@ -2,15 +2,34 @@
 #define IMAGE_H_INCLUDED
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 
-template <class VoxelType> class Image {
+template <class DataType> class DataItem {
+	protected:
+		std::vector<DataType> data;
+	public:
+		// Get the value with the specific index.
+		DataType getData(size_t i) const {
+			return data[i];
+		}
+
+		// Set the value with the specific index.
+		void setData(size_t i, DataType value) {
+			data[i] = value;
+		}
+
+		size_t getSize(void) const {
+			return data.size();
+		};
+};
+
+template <class VoxelType> class Image: public DataItem<VoxelType> {
 	private:
 		size_t width;
 		size_t height;
 		size_t depth;
 
-		std::vector<VoxelType> data;
 	public:
 		size_t getWidth(void) const {
 			return width;
@@ -21,16 +40,13 @@ template <class VoxelType> class Image {
 		size_t getDepth(void) const {
 			return depth;
 		};
-		size_t getSize(void) const {
-			return width * height * depth;
-		};
 
 		// Parametric constructor.
 		Image(size_t aWidth, size_t aHeight, size_t aDepth) {
 			width = aWidth;
 			height = aHeight;
 			depth = aDepth;
-			data.resize(width * height * depth);
+			this->data.resize(width * height * depth);
 		}
 
 		// Load from file constructor.
@@ -40,36 +56,57 @@ template <class VoxelType> class Image {
 
 		// Get the value of a voxel with the specific index.
 		VoxelType getVoxel(size_t i) const {
-			return data[i];
+			//return data[i];
+			return this->getData(i);
 		}
 
 		// Get the value of a voxel with the specific coords.
 		VoxelType getVoxel(size_t x, size_t y, size_t z) const {
-			return data[x + getWidth() * y + getWidth() * getHeight() * z];
+			//return data[x + getWidth() * y + getWidth() * getHeight() * z];
+			return getData(x + getWidth() * y + getWidth() * getHeight() * z);
 		}
 
 		// Set the value of a voxel with the specific index.
 		void setVoxel(size_t i, VoxelType value) {
-			data[i] = value;
+			//data[i] = value;
+			setData(i, value);
 		}
 
 		// Set the value of a voxel with the specific coords.
 		void setVoxel(size_t x, size_t y, size_t z, VoxelType value) {
-			data[x + getWidth() * y + getWidth() * getHeight() * z] = value;
+			//data[x + getWidth() * y + getWidth() * getHeight() * z] = value;
+			setData(x + getWidth() * y + getWidth() * getHeight() * z, value);
 		}
 
 
 		// \todo Load the image from a fie.
 		void loadFromFile(const std::string &fileName) {
+			std::ifstream input(fileName);
+
+			input >> this->width;
+			input >> this->height;
+			input >> this->depth;
+			this->data.resize(width * height * depth);
+
+			for (size_t i = 0; i < this->data.size(); ++i) {
+				VoxelType voxel;
+				input >> voxel;
+				setVoxel(i, voxel);
+			}
+
+			input.close();
+
+			/* \todo Remove this as soon as the proper image loading (above) is tested.
 			width = 10;  // \todo Read the image width.
 			height = 10; // \todo Read the image height.
 			depth = 10;  // \todo Read the image depth.
 
-			data.resize(width * height * depth);
+			this->data.resize(width * height * depth);
 
-			for (size_t i = 0; i < data.size(); ++i) {
-				data[i] = 0.1; // \todo Read the value of the i-th voxel.
+			for (size_t i = 0; i < this->data.size(); ++i) {
+				setVoxel[i] = 0.1; // \todo Read the value of the i-th voxel.
 			}
+			*/
 		}
 
 };
